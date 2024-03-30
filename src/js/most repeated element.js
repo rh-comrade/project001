0e14233ec2074a70e1ee55c8394c406bad0799f0
Rhutik Chaudhari <rhutikchaudhari662@gmail.com>
2024-03-30 23:47:41 +0530

    most repeated code optimized

--- src/js/most repeated element.js
+++ src/js/most repeated element.js
@@ -1,6 +1,4 @@
 var frequency = {};
-var mk;
-var mv = 0;
 
 var array=[1,2,3,4,1,8,2,7,2,3,2,3,4,5,2]
 
@@ -10,12 +8,5 @@ array.map((value)=>{
 })
 
 // find maximum
-for(var key in frequency){
-    var currentValue = frequency[key];
-    if(currentValue > mv){
-        mv = currentValue;
-        mk = key; // this is what we required
-    }
-}
-
-console.log(mk)
+mx = Math.max(...Object.values(frequency))
+console.log(mx);
