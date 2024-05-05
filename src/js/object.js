f300336cfbae56837d6e4e4ec2d3b4b3b25299fc
Rhutik Chaudhari <rhutikchaudhari662@gmail.com>
2024-05-05 21:51:33 +0530

    us001: object updated with preventExtensions method

--- src/js/object.js
+++ src/js/object.js
@@ -14,7 +14,15 @@ var obj2 = obj;
 
 // method 2
 // if we seal object then we can only able to update object. other operation is not allowed
-Object.seal(obj)
-obj.name = 'dhoni'
-// obj2.name = 'gangully'
-console.log(obj.name)
+// Object.seal(obj)
+// obj.name = 'dhoni'
+// // obj2.name = 'gangully'
+// console.log(obj.name)
+
+// prevent extension
+// if we do prevent extension on object then we can only able to update and delete data other operation is disabled/not allowed
+
+Object.preventExtensions(obj)
+obj.name = "test001"
+// obj.runs = 5000; //not allowed
+console.log(obj)
